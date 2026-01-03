c79be57e422c580774398780b3e3ff9019c1f6c7
scott-a11y <scott@scottdlittle.com>
2026-01-03 13:46:24 -0800

    Fix: Add 'use client' directive to specs page for React hooks

--- app/specs/page.tsx
+++ app/specs/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState, useEffect } from "react";
 import { extractMachinerySpecs } from "@/actions/extract-specs";
 import { saveMachinerySpecs } from "@/actions/save-specs";
